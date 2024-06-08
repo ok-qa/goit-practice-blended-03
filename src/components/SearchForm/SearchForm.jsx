267a00242c36f133ee5b7e3332c5192f584063d2
Oleksandra <sashakirina@gmail.com>
2024-06-08 15:21:50 +0200

    2 task

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,45 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import styles from './SearchForm.module.css';
 
-//const regions = [
-//   { id: 'africa', value: 'africa', name: 'Africa' },
-//   { id: 'americas', value: 'americas', name: 'America' },
-//   { id: 'asia', value: 'asia', name: 'Asia' },
-//   { id: 'europe', value: 'europe', name: 'Europe' },
-//   { id: 'oceania', value: 'oceania', name: 'Oceania' },
-// ];
+const regions = [
+  { id: 'africa', value: 'africa', name: 'Africa' },
+  { id: 'americas', value: 'americas', name: 'America' },
+  { id: 'asia', value: 'asia', name: 'Asia' },
+  { id: 'europe', value: 'europe', name: 'Europe' },
+  { id: 'oceania', value: 'oceania', name: 'Oceania' },
+];
 
-export const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+export const SearchForm = ({ onSubmit }) => {
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const value = evt.target.elements.region.value;
+    onSubmit(value);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <button className={styles.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <select
+        aria-label="select"
+        className={styles.select}
+        name="region"
+        required
+        defaultValue="default"
+      >
+        <option disabled value="default">
+          Select a region
+        </option>
+        {regions.map(({ id, value, name }) => {
+          return (
+            <option key={id} value={value}>
+              {name}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
 };
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,32 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, SearchForm, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/countryApi';
 
 export const SearchCountry = () => {
+  const [query, setQuery] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    if (!query) return;
+    const getData = async () => {
+      try {
+        const countries = await fetchByRegion(query);
+        setCountries(countries);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [query]);
+
+  const onSubmit = value => {
+    setQuery(value);
+  };
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
